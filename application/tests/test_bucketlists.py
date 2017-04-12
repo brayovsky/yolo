055afbffe942264da005c2e1e82f8c15cbf9345e
brayovsky <brian.muthui@andela.com>
2017-04-12 11:39:12 +0300

    [feature 143118259] Add tests for Bucketlists model methods

---
+++ application/tests/test_bucketlists.py
@@ -0,0 +1,12 @@
+from application.tests.base_test import BaseTestCase
+
+
+class TestBucketlists(BaseTestCase):
+    def setUp(self):
+        super(TestBucketlists, self).setUp()
+
+    def tearDown(self):
+        pass
+
+    def test_updates_date_modified(self):
+        pass
