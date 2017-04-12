0f25ed325ce17a7234661a7bbe8b62e6d3d1c991
brayovsky <brian.muthui@andela.com>
2017-04-12 11:02:48 +0300

    [feature 143118259] Add test definitions for the route /bucketlists/<id>/items/

--- application/tests/test_bucketlistitems.py
+++ application/tests/test_bucketlistitems.py
@@ -9,19 +9,10 @@ class TestBucketlistItems(BaseTestCase):
     def tearDown(self):
         pass
 
-    def test_must_be_logged_in_to_create_items(self):
-        pass
-
     def test_must_be_logged_in_to_update_items(self):
         pass
 
     def test_must_be_logged_in_to_delete_items(self):
-        pass
-
-    def test_does_not_create_items_with_same_name(self):
-        pass
-
-    def test_creates_items(self):
         pass
 
     def test_does_not_update_non_existent_items(self):
