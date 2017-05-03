e0d6caf9500edf19e9252eec482ed9a998dedd9e
brayovsky <brian.muthui@andela.com>
2017-05-03 12:47:27 +0300

    [ft 144635851] Add signup controller variables and methods

--- application/main/static/js/yolo-controllers.js
+++ application/main/static/js/yolo-controllers.js
@@ -5,22 +5,39 @@ var yoloControllers = angular.module('yoloControllers', []);
 yoloControllers.controller('MainCtrl', ['$scope','$http',
     function MainCtrl($scope, $http) {
         $scope.appName = 'yolo';
-        $scope.hideLogin = false;
-        $scope.hideSignup = false;
+        $scope.showLogin = false;
+        $scope.showSignup = false;
+        $scope.userExists = false;
+
+        // Highlight inputs if they have errors
+        $scope.highlightInput = function(inputid){
+            $('#' + inputid).css('border', '1px solid #f00');
+        }
 
         // Toggle login form
         $scope.toggleLogin = function(){
-            $scope.hideLogin = !$scope.hideLogin;
-            $scope.hideSignup = $scope.hideSignup ? !$scope.hideSignup : $scope.hideSignup;
+            $scope.showLogin = !$scope.showLogin;
+            $scope.showSignup = $scope.showSignup ? !$scope.showSignup : $scope.showSignup;
         };
         // Toggle signup form
         $scope.toggleSignup = function(){
-            $scope.hideSignup = !$scope.hideSignup;
-            $scope.hideLogin = $scope.hideLogin ? !$scope.hideLogin : $scope.hideLogin;
+            $scope.showSignup = !$scope.showSignup;
+            $scope.showLogin = $scope.showLogin ? !$scope.showLogin : $scope.showLogin;
          };
 
          // Access register endpoint
         $scope.yoloRegister = function(){
+            // Check passwords match
+            if ($scope.signupPassword !== $scope.signupRepeatPassword){
+                // Highlight errors
+                $scope.highlightInput('signup-password');
+                $scope.highlightInput('signup-repeat-password');
+
+                // Display message
+                $scope.errors.signupRepeatPassword = ["Passwords don't match"];
+                return
+            }
+            // Access API
             $http({
                 method : 'POST',
                 url : 'api/v1/auth/register',
@@ -39,9 +56,24 @@ yoloControllers.controller('MainCtrl', ['$scope','$http',
             $scope.yoloLogin();
         },
         function error(response){
-            // Highlight errors
-            if (response.status_code == 400){  // Form errors
-
+            // Highlight new errors
+            if (response.status == 400){  // Form errors
+                 if (response.data.username){
+                    $scope.highlightInput('signup-username');
+                 }
+                 if (response.data.email){
+                    $scope.highlightInput('signup-email');
+                    $scope.errors.signupEmail = response.data.email;
+                 }
+                 if (response.data.password){
+                    $scope.highlightInput('signup-password');
+                 }
+            }
+            else if (response.status == 403){ // User exists
+                // Go to login
+                $scope.userExists = true;
+                $scope.showSignup = false;
+                $scope.showLogin = true;
             }
         });
         };
