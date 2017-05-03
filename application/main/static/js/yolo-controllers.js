d247100b0190604c5f168eb35595f8d027ca27ac
brayovsky <brian.muthui@andela.com>
2017-05-03 08:09:18 +0300

    [ft 144635851] Add http service for register and login api endpoints

--- application/main/static/js/yolo-controllers.js
+++ application/main/static/js/yolo-controllers.js
@@ -2,19 +2,70 @@
 /* Controllers */
 var yoloControllers = angular.module('yoloControllers', []);
 
-yoloControllers.controller('MainCtrl', ['$scope',
-    function MainCtrl($scope) {
-        $scope.appName = "yolo";
+yoloControllers.controller('MainCtrl', ['$scope','$http',
+    function MainCtrl($scope, $http) {
+        $scope.appName = 'yolo';
         $scope.hideLogin = false;
         $scope.hideSignup = false;
+
+        // Toggle login form
         $scope.toggleLogin = function(){
-                        $scope.hideLogin = !$scope.hideLogin;
-                        $scope.hideSignup = $scope.hideSignup ? !$scope.hideSignup : $scope.hideSignup;
-                        };
+            $scope.hideLogin = !$scope.hideLogin;
+            $scope.hideSignup = $scope.hideSignup ? !$scope.hideSignup : $scope.hideSignup;
+        };
+        // Toggle signup form
         $scope.toggleSignup = function(){
-                        $scope.hideSignup = !$scope.hideSignup;
-                        $scope.hideLogin = $scope.hideLogin ? !$scope.hideLogin : $scope.hideLogin;
-                        };
+            $scope.hideSignup = !$scope.hideSignup;
+            $scope.hideLogin = $scope.hideLogin ? !$scope.hideLogin : $scope.hideLogin;
+         };
+
+         // Access register endpoint
+        $scope.yoloRegister = function(){
+            $http({
+                method : 'POST',
+                url : 'api/v1/auth/register',
+                data: $.param({
+                    'username': $scope.signupUsername,
+                    'email': $scope.signupEmail,
+                    'password': $scope.signupPassword
+                }),
+                headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }).then(function success(response) {
+            // Log in user
+            $scope.loginUsername = response.data.username;
+            $scope.loginPassword = $scope.signupPassword;
+            $scope.yoloLogin();
+        },
+        function error(response){
+            // Highlight errors
+            if (response.status_code == 400){  // Form errors
+
+            }
+        });
+        };
+
+        // Access login endpoint
+        $scope.yoloLogin = function(){
+            $http({
+                method : 'POST',
+                url : 'api/v1/auth/login',
+                data: $.param({
+                    'username': $scope.loginUsername,
+                    'password': $scope.loginPassword
+                }),
+                headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }).then(function success(response) {
+            // Switch to dashboard
+            console.log(response.data)
+        },
+        function error(response){
+            // Highlight form errors
+        });
+        };
     }]);
 
 yoloControllers.filter('capitalize', function() {
